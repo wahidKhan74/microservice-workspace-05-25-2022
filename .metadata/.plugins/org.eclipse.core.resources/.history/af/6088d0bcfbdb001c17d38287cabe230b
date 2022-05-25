67d415ed6463ac3bf85cb694f814226ceb60b577
Waheed <wahid.khan74@gmail.com>
2022-05-25 07:34:14 +0000

    added micro service setup

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/af/6088d0bcfbdb001c17d38287cabe230b
@@ -0,0 +1,10 @@
+package com.simplilearn.accounts.exception;
+
+public class AccountNotFound extends RuntimeException {
+	
+	private static final long serialVersionUID = 1L;
+
+	public AccountNotFound(String message){
+		super(message);
+	}
+}
---
+++ api-gateway/src/test/java/com/simplilearn/gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
